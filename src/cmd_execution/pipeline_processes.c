40e126b3a0ffdf00105ff1dcc220c4b2bcc40f62
David Labrador Gracia <dalabrad@c4r8s1.42urduliz.com>
2025-09-26 18:28:40 +0200

    Now working the cases when a redirection to a file appears between pipes:
    	modified:   src/cmd_execution/pipeline_processes.c

--- src/cmd_execution/pipeline_processes.c
+++ src/cmd_execution/pipeline_processes.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipeline_processes.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vlorenzo <vlorenzo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dalabrad <dalabrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/28 17:18:35 by dalabrad          #+#    #+#             */
-/*   Updated: 2025/09/26 17:36:01 by vlorenzo         ###   ########.fr       */
+/*   Updated: 2025/09/26 18:19:24 by dalabrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static void	close_safe(int *fd)
 	}
 }
 
-static void	child_stdio(t_data *d, size_t i)
+static void	child_stdio(t_data *d, t_cmd *cmd, size_t i)
 {
 	int	read_fd;
 	int	write_fd;
@@ -37,9 +37,9 @@ static void	child_stdio(t_data *d, size_t i)
 		write_fd = -1;
 	else
 		write_fd = d->pipes[i % 2][W_PIPE];
-	if (read_fd >= 0)
+	if (read_fd >= 0 && !cmd->file_in)
 		dup2(read_fd, STDIN_FILENO);
-	if (write_fd >= 0)
+	if (write_fd >= 0 && !cmd->file_out)
 		dup2(write_fd, STDOUT_FILENO);
 	close_safe(&d->pipes[0][R_PIPE]);
 	close_safe(&d->pipes[0][W_PIPE]);
@@ -51,7 +51,7 @@ void	child_process(t_data *data, t_cmd *cmd, size_t i)
 {
 	if (file_in_redir(cmd) < 0 || file_out_redir(cmd) < 0)
 		exit(1);
-	child_stdio(data, i);
+	child_stdio(data, cmd, i);
 	command_exec(cmd, data);
 	exit(g_status);
 }
